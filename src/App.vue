36445f807dc828f33d71b8a92c6cc6f670853914
Ben Dirix <benjamindirix@hotmail.com>
2019-03-13 06:28:11 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">      
+    <quotegrid :quotes="quotes"></quotegrid>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+import Quotegrid from './components/Quotegrid.vue';
+    export default {
+        data(){
+          return{
+            quotes: ['First quote']
+          }            
+        },
+        components:{
+            Quotegrid,
+        }
+        
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Quotegrid.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <Quote v-for="quote in quotes" :key="quote.id" :quotes="quotes">{{quote}}</Quote>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+export default {
+  props: ["quotes"],
+  components:{
+    Quote,
+  }
+};
+</script>
+
+<style>
+</style>
